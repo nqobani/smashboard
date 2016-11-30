ce1a8fc76d7c52c756248e973ff3a2962bfb446a
nqobani <nqobanizulu15@gmail.com>
2016-11-30 14:58:21 +0200

    Removed The documentation code

--- Part1.Api/Part1.Api/App_Start/SwaggerConfig.cs
+++ Part1.Api/Part1.Api/App_Start/SwaggerConfig.cs
@@ -153,8 +153,7 @@ namespace Part1.Api
                         // Xml comments (http://msdn.microsoft.com/en-us/library/b2s063f7(v=vs.110).aspx), you can incorporate
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
-                        //
-                        c.IncludeXmlComments(GetXmlCommentsPath());
+                        //c.IncludeXmlComments(GetXmlCommentsPath());
 
                         // In contrast to WebApi, Swagger 2.0 does not include the query string component when mapping a URL
                         // to an action. As a result, Swashbuckle will raise an exception if it encounters multiple actions
